d6dd6bcac4b45530bfd57923f65d22c31521f1e6
Ethan Joachim Eldridge <ejayeldridge@gmail.com>
2013-05-12 17:50:50 -0400

    Using webpage as hash key to maintain visited links, not sure if just using links will be unique enough that sites wont be visited twice (relative paths could mess those up). Added depth parameter to httpGet

--- main.js
+++ main.js
@@ -31,48 +31,74 @@ if (canvas && canvas.getContext) {
 //Use a GLOBAL json object to stored the results of our searchs, then we can just render that to the screen
 var nodes = [];
 
+//Use a hash to keep track of unique links, we don't want to process the same link twice
+var visitedLinks = {}
+
+
 //Define the object we'll use for each node
 function node(){
 	this.title = "";
 	this.anchors = [];
 }
 
-//Processing function for each URL. Parses the page and grabs out the title and tags
-function processGET(xmlHttp){
+//Processing function for each URL. Parses the page and grabs out the title and tags.
+function processGET(xmlHttp,depth,baseURL){
 	if ( xmlHttp.readyState == 4 && xmlHttp.status == 200 ){
         var pageAsText = xmlHttp.responseText;
         
         var anchorMatch = /<a.*href="([^"]*).*<\/a>/ig;
-        var titleMatch = /<title>(.*)<\/title>/i;
+        var titleMatch = /<title>(.*)<\/title>/im;
 
         
         //Grab the title
         var result = new node();
-        result.title = titleMatch.exec(pageAsText)[1]; //exec returns 0=>whole match 1=>what I want in ()
-
+        var tmp = titleMatch.exec(pageAsText); //exec returns 0=>whole match 1=>what I want in ()
+        if(tmp != null)
+        	result.title = tmp[1];
+        else
+        	return;//Don't care if theres no title, I dont want this page then
         
         //Loop through the text and find all
         var intermediate;
         do{
 			intermediate = anchorMatch.exec(pageAsText);	
 			if(intermediate)
-				result.anchors.push(intermediate[1]);
+				if(!visitedLinks[intermediate]){
+					//If the anchor text doesn't begin with http, its a relative path and we should base it off the url
+					if(intermediate[1].indexOf(baseURL)==-1){
+						intermediate[1] = baseURL + intermediate[1];
+					}
+					result.anchors.push(intermediate[1]);
+					visitedLinks[intermediate] = true;
+				}else{
+					//We've visited this link before
+				}
+
     	}while(intermediate != null);    	
+
+    	//For each of these anchors we've found, we want to grab their links too, if the depth is permissiable
+    	if(depth > 0){
+    		for (var i = result.anchors.length - 1; i >= 0; i--) {
+    			httpGet(encodeURIComponent(result.anchors[i]),depth-1);
+    		};
+    	}
 		
+		//Add this result to the global nodes list
+		nodes.push(result);
 
-		console.log(result);
+		console.log(nodes);
 
+		//Set some flag to update the graph
     }
 }
 
 //Helper function to grab URL
-function httpGet(theUrl){
+function httpGet(theUrl,depth){
     var xmlHttp = null;
     xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function(){processGET(xmlHttp)};
-    xmlHttp.open( "GET", theUrl, true );
+    xmlHttp.onreadystatechange = function(){processGET(xmlHttp,depth,decodeURIComponent(theUrl))};
+    xmlHttp.open( "GET", 'proxy.php?url='+theUrl, true );
     xmlHttp.send( null );
-    return xmlHttp.responseText;
 }
 
 //The function that will go and do all the work
@@ -85,7 +111,7 @@ function aggregate(){
 	 * Draw links to each from root
 	 * Update
 	 */
-	 httpGet('proxy.php?url='+encodeURIComponent(rootUrl));
+	 httpGet(encodeURIComponent(rootUrl),3);
 	
 }
 
